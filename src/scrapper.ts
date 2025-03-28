0c1b110eab5ba17414aad21217dd3835cd72e649
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2025-03-28 15:33:20 -0300

    Scrapper está pegando o resumo do link

--- src/scrapper.ts
+++ src/scrapper.ts
@@ -5,6 +5,7 @@ const url: string = "https://www.iihf.com/en/news";
 
 export interface ObjetoNoticia {
   titulo: string | null;
+  resumo: string | null;
   link: string | null;
   img: string | null;
 }
@@ -16,20 +17,20 @@ export async function GetHockeyNews(): Promise<ObjetoNoticia[]> {
 
   //Pegamos o Link,Titulo e Imagem
   $("div.b-card").each((_, elemento) => {
-    const link = url.slice(0,20) + $(elemento).find("a").attr("href") || null;
+    const link = url.slice(0, 20) + $(elemento).find("a").attr("href") || null;
     const titulo = $(elemento).find("div.s-text-container").find("a").find("div.s-title").text();
+    const resumo = $(elemento).find("div.s-text-container").find("a").text();
     // Extrai a URL da imagem usando regex ou substring
     const styleAttr = $(elemento).find("a.s-image").attr("style") || '';
     // Usando regex para removermos o conteúdo que está ao redor do https://blob...
     const imgMatch = styleAttr.match(/url\(['"]?([^'"]+)['"]?\)/);
-    const img = imgMatch ? imgMatch[1] : null;      
-    retorno.push({ titulo, link, img })
+    const img = imgMatch ? imgMatch[1] : null;
+    retorno.push({ titulo, resumo, link, img })
   });
   return retorno;
 }
 
-
-// style="background-image:url('https://blob.iihf.com/iihf-media/iihfmvc/media/2025/wm18/wm18/news/landon_dupont.jpg')"
-
 //rode com npx vite-node
-console.log(url.slice(0,21))
+
+const x = await GetHockeyNews()
+console.log(x);
