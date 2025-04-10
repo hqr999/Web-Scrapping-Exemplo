d3aaf3556432706227a3e2738fb8f2b8580dc237
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2025-04-10 19:57:21 -0300

    Scrapper do golf terminado

--- src/scrapper_golf.ts
+++ src/scrapper_golf.ts
@@ -1,38 +1,27 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 
-const url: string = "https://www.golf.com/news";
-
+const url: string = "https://www.golfdigest.com/golf-news"
 export interface ObjetoNoticiaGolf {
   titulo: string | null;
-  autor: string | null;
   link: string | null;
   img: string | null;
 }
 
-const r = await axios.get(url);
-const $ = cheerio.load(r.data);
-console.log($("div.latest-in-category__list").get());
-
-/*
 export async function GetGolfNews(): Promise<ObjetoNoticiaGolf[]> {
   const resp = await axios.get(url);
   const $ = cheerio.load(resp.data);
-  const retorno:ObjetoNoticiaGolf[] = [];
+  const retorno: ObjetoNoticiaGolf[] = [];
+  $('.storyCard').each((_,elem) => {
+    console.log("ENTREI AQUI!!")
+    const titulo = $(elem).find('a').find('span').text()
+    const link = $(elem).find('a').attr("href") || null 
+    const img = $(elem).find('a').find('img').attr('src') || null 
 
-  $('article.latest-in-category__article').each((_,elem) => {
-     const titulo = $(elem).find('a').find('h3').text();
-     const autor = $(elem).find('div.latest-in-category__by-line').find('a').text();
-
-     const link = $(elem).find('a').attr('href') || null;
-     const img = $(elem).find('a').find('div.latest-in-category__image-wrapper').find('img').attr('src') || null;
-
-    retorno.push({titulo,autor,link,img})
+    retorno.push({titulo,link,img})
   })
-
-  return retorno;
-}
-
+  return retorno
+  }
 
 const x = await GetGolfNews();
-console.log(x);*/
+console.log(x);
